a46445fd031a19c634dbe3d7ea564b5edae54d7d
supremos2366 <derrickwadek@gmail.com>
2022-10-11 18:07:05 +0300

    Find a number in numbers (using O(log n)

--- src/Search.kt
+++ src/Search.kt
@@ -11,5 +11,26 @@ fun ifContainsValue(value: Int, numbers: List<Int>): Boolean{
 
 
 fun main(args: Array<String>) {
-    println(ifContainsValue(80, numbers))
+    println(ifContainsValue(90, numbers))
+    println(ifContainsValueImproved(105,numbers))
+}
+
+//To improve on this, we can use the logarithmic time complexity O(log n)
+//If you were checking if the number 451 existed in the list, this algorithm would have
+//to iterate from the beginning to end, making a total of nine inspections for the nine
+//values in the list. However, since the list is sorted, you can, right off the bat, drop half
+//of the comparisons necessary by checking the middle value:
+
+fun ifContainsValueImproved(value: Int, numbers: List<Int>):Boolean{
+    //check middle index
+    val middleIndex = numbers.size/2
+    if (value <= numbers[middleIndex])
+        for (index in 0..middleIndex){
+            if (numbers[index] == value) return true
+        } else {
+            for (index in middleIndex until numbers.size){
+                if (numbers[index] == value) return true
+            }
+        }
+    return false
 }
